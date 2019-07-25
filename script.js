67875154bf7f40cfa19365beb76c47f81c8095d1
Jill Williams <codeGeek.03.14@gmail.com>
2019-07-25 11:58:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// const addPolynomials = ( ) => {
+//     var a = document.getElementById("mya").value;
+//     var b = document.getElementById("myb").value;
+//     var c = document.getElementById("myc").value;
+//     var d = document.getElementById("myd").value;
+//     var answer1 = a + c;
+//     var answer2 = b + d;
+
+//     if (a == 0 && b == 0 && c == 0 && d == 0) {
+//         return "Re-enter your values";
+//     } else 
+//     if (b+d < 0) {
+//         return  answer1 + answer2 + " i";
+//     } else 
+//         return answer1 + " + " + answer2 + " i";
+// }
+// const answer = ( ) => {
+//     document.getElementById("demo").innerHTML = addPolynomials( );
+// };
+
+// ADD POLYNOMIALS SECTION
+const addPolynomials = ( )=> {
+    var a = document.getElementById("mya").value;
+    var b = document.getElementById("myb").value;
+    var c = document.getElementById("myc").value;
+    var d = document.getElementById("myd").value;
+    var answer1 = a + c;
+    var answer2 = b + d;
+
+    if (a == 0 && b == 0 && c == 0 && d == 0) {
+        return "Re-enter your values";
+    } else 
+    if (b+d < 0) {
+        return answer1 + answer2 + " i";
+    } else 
+        return answer1 + " + " + answer2 + " i";
+}
+const answer = ( ) => {
+    document.getElementById("demo").innerHTML = addPolynomials( );
+};
+
+// SUBTRACT POLYNOMIALS SECTION
+const subtractPolynomials = ( )=> {
+    var a2 = document.getElementById("a2").value;
+    var b2 = document.getElementById("b2").value;
+    var c2 = document.getElementById("c2").value;
+    var d2 = document.getElementById("d2").value;
+    var answer3 = a2 - c2;
+    var answer4 = b2 - d2;
+
+    if (a2 == 0 && b2== 0 && c2 == 0 && d2 == 0) {
+        return "Re-enter your values";
+    } else 
+    if (b2-d2 < 0) {
+        return answer3 + answer4 + " i";
+    } else 
+        return answer3 + " + " + answer4 + " i";
+}
+const answer2= ( ) => {
+    document.getElementById("demo2").innerHTML = subtractPolynomials( );
+};
